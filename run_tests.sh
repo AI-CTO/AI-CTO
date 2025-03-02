0d6474eb8135717cc9886fa45e785692d53433a7
modar_algayal <mudar.algayal@helsinki.fi>
2025-03-02 12:39:40 +0200

    added to CI

--- run_tests.sh
+++ run_tests.sh
@@ -20,7 +20,6 @@ echo "✅ Sovellus käynnissä!"
 
 echo "Ajetaan Robot Framework -testit..."
 poetry run robot tests/
-pytest tests/
 
 echo "Pysäytetään sovellus..."
 kill $APP_PID || echo "⚠️ Prosessi $APP_PID ei ollut käynnissä."
