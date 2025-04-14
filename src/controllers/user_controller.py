ce7f24d80db9c7c1945d775bab544aaa18597472
Kirke Ruusalu <kirke.ruusalu@gmail.com>
2025-04-14 03:30:02 +0300

    created branch and fixed some pylint errors

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,15 +1,20 @@
 from flask import Blueprint, jsonify, request
-from models.models import User, db
+from models.models import db, User
 
 user_bp = Blueprint('user', __name__)
 
 @user_bp.route("/add_user", methods=["POST"])
 def add_user():
+    """Function for creating a user
+
+    Returns:
+        JSON message
+    """
     try:
         data = request.get_json()
         new_user = User(**data)
         db.session.add(new_user)
         db.session.commit()
         return jsonify({"message": "User added!", "user": new_user.to_dict()}), 201
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-except
         return jsonify({"error": "Failed to add user", "details": str(e)}), 500
